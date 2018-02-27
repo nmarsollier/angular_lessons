a598cd25ff0ca2ff24c29b3418253d2903399d21
nmarsollier <nmarsollier@gmail.com>
2018-02-27 10:40:03 -0300

    Actualizando hola mundo

--- holaMundo/src/main.ts
+++ holaMundo/src/main.ts
@@ -1,11 +1,12 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
-import { environment } from './environments/environment';
-import { HolaMundoModule } from './holaMundo/holaMundo.module';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(HolaMundoModule);
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.log(err));
